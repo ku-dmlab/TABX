fix: mask: fix action mask for buyable units

src/maenv/tabs/tabs_unit_comb/tabs_unit_comb.py
@@ -60,7 +60,7 @@ class TABSUnitComb(BaseMAEnv):
             current_unit_list=jnp.zeros(self.max_num_units, dtype=jnp.int32),
             enemy_unit_comp=scenario.enemy_unit_comp,
             unit_comp_mask=scenario.unit_comp_mask,
-            action_mask=action_mask,
+            action_mask=action_mask.astype(jnp.float32),
             scenario=scenario,
         )
         return self.get_obs(state), state
@@ -80,7 +80,7 @@ class TABSUnitComb(BaseMAEnv):
 
         max_agent_mask = jnp.where(new_unit_list < self.max_agents, 1, 0)
         action_mask = (
-            jnp.where(new_budget >= state.all_price, True, False)
+            jnp.where(budget >= state.all_price, True, False)
             * state.unit_comp_mask
             * max_agent_mask
         )
@@ -88,7 +88,7 @@ class TABSUnitComb(BaseMAEnv):
         state = state.replace(
             budget=budget,
             current_unit_list=new_unit_list.astype(jnp.int32),
-            action_mask=action_mask,
+            action_mask=action_mask.astype(jnp.float32),
         )
         # NOTE: Reward would be computed by the result of battle with this unit combination.
         reward = None

src/maenv/tabs/test/test_tabs_unit_comb.py
@@ -1,24 +1,31 @@
 import unittest
 
+import jax
 import jax.numpy as jnp
 
 from src.maenv.tabs.tabs_unit_comb.tabs_unit_comb import TABSUnitComb
 from src.maenv.tabs.units import get_all_unit_names
-from src.maenv.tabs.scenarios import TABSConf, Scenario, generate_scenario
+from src.maenv.tabs.scenarios import TABSConf, Scenario, generate_scenario, default_tabs_conf
 
 
 class TestTABSUnitComb(unittest.TestCase):
     def test_reset(self):
-        self.reset_predefined_scenario1()
+        self.reset_predefined_scenario1("20farmers")
+        self.reset_predefined_scenario1("1theking")
+        self.reset_predefined_scenario1("4archer_1mammoth")
 
-    def reset_predefined_scenario1(self):
+    def test_purchase(self):
+        rng = jax.random.PRNGKey(0)
+        rng, _rng = jax.random.split(rng)
+        self.purchase1(_rng)
+
+    def reset_predefined_scenario1(self, scenario_name):
         tabs_conf = TABSConf(
-            scenario_name="4archer_1mammoth",
+            scenario_name=scenario_name,
             max_agents=20,
             max_num_units=len(get_all_unit_names()),
             max_field_height=4,
             max_field_width=5,
-            scenario=None,
         )
         env = TABSUnitComb(tabs_conf)
         scenario = generate_scenario(tabs_conf)
@@ -33,6 +40,50 @@ class TestTABSUnitComb(unittest.TestCase):
             )
         )
         self.assertTrue(jnp.array_equal(o, expected_obs))
+
+    def purchase1(self, rng):
+        env = TABSUnitComb(default_tabs_conf)
+        scenario = generate_scenario(default_tabs_conf)
+
+        rng, _rng = jax.random.split(rng)
+        o, s = env.reset(rng, scenario)
+        action = jnp.array(3)  # buy BombThrower
+        o, s, _, _, _ = env.step(_rng, s, action)
+
+        self.assertTrue(
+            s.budget == scenario.budget - scenario.price[action],
+            f"\ntrue: {scenario.budget - scenario.price[action]}\nout: {s.budget}",
+        )
+        true = jnp.array([0, 0, 0, 1, 0, 0, 0], dtype=jnp.int32)
+        self.assertTrue(
+            jnp.array_equal(s.current_unit_list, true),
+            f"\ntrue: {true}\nout: {s.current_unit_list}",
+        )
+        print(s.budget)
+        true = jnp.array([1, 1, 0, 1, 0, 1, 1], dtype=jnp.float32)
+        self.assertTrue(
+            jnp.array_equal(s.action_mask, true),
+            f"\ntrue: {true}\nout: {s.action_mask}",
+        )
+
+    def purchase2(self, rng):
+        env = TABSUnitComb(default_tabs_conf)
+        scenario = generate_scenario(default_tabs_conf)
+
+        rng, _rng = jax.random.split(rng)
+        o, s = env.reset(rng, scenario)
+        action = jnp.array(5)
+        o, s, _, _, _ = env.step(_rng, s, action)
+
+        self.assertTrue(s.budget == scenario.budget, f"\ntrue: {scenario.budget}\nout: {s.budget}")
+        self.assertTrue(
+            jnp.array_equal(s.current_unit_list, scenario.current_unit_list),
+            f"\ntrue: {scenario.current_unit_list}\nout: {s.current_unit_list}",
+        )
+        self.assertTrue(
+            jnp.array_equal(s.action_mask, scenario.action_mask),
+            f"\ntrue: {scenario.action_mask}\nout: {s.action_mask}",
+        )
 
 
 if __name__ == "__main__":