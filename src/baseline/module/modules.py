b6588969a1867d58d609d4a61a0181d31ebcfeae
hy-kiera <bb.kiera@gmail.com>
2025-09-05 07:07:58 +0000

    feat: algo: implement ippo and training

--- src/baseline/module/modules.py
+++ src/baseline/module/modules.py
@@ -50,13 +50,9 @@ class RNNCritic(nnx.Module):
             nnx.Linear(state_dim, layer_dim, rngs=rngs), nnx.LayerNorm(layer_dim, rngs=rngs)
         )
         self.gru = nnx.GRUCell(layer_dim, layer_dim, rngs=rngs)
-        self.value = nnx.Sequential(
+        self.critic = nnx.Sequential(
             nnx.relu,
-            nnx.Linear(
-                layer_dim,
-                layer_dim,
-                rngs=rngs,
-            ),
+            nnx.Linear(layer_dim, layer_dim, rngs=rngs),
             nnx.LayerNorm(layer_dim, rngs=rngs),
             nnx.relu,
             nnx.Linear(
@@ -71,7 +67,7 @@ class RNNCritic(nnx.Module):
         # observation shape = (batch_size, observation_dim)
         obs_emb = self.layer(observation)  # (batch_size, layer_dim)
         next_state, output = self.gru(hidden_state, obs_emb)
-        return next_state, self.value(output)
+        return next_state, self.critic(output)
 
     def initialize_carry(self, shape):
         return self.gru.initialize_carry(shape)
@@ -86,11 +82,7 @@ class RNNHybridPolicy(nnx.Module):
         self.gru = nnx.GRUCell(layer_dim, layer_dim, rngs=rngs)
         self.policy = nnx.Sequential(
             nnx.relu,
-            nnx.Linear(
-                layer_dim,
-                layer_dim,
-                rngs=rngs,
-            ),
+            nnx.Linear(layer_dim, layer_dim, rngs=rngs),
             nnx.LayerNorm(layer_dim, rngs=rngs),
             nnx.relu,
         )
@@ -127,6 +119,64 @@ class RNNHybridPolicy(nnx.Module):
         std = self.policy_std(output)
 
         return next_state, logits, mean, std
+
+    def initialize_carry(self, shape):
+        return self.gru.initialize_carry(shape)
+
+
+class RNNActorCritic(nnx.Module):
+    def __init__(self, obs_dim, action_dim, layer_dim, rngs=nnx.Rngs(0)):
+        self.layer = nnx.Sequential(
+            nnx.Linear(obs_dim, layer_dim, rngs=rngs),
+            nnx.LayerNorm(layer_dim, rngs=rngs),
+            nnx.relu,
+        )
+        self.gru = nnx.GRUCell(layer_dim, layer_dim, rngs=rngs)
+        self.actor = nnx.Sequential(
+            nnx.relu,
+            nnx.Linear(layer_dim, layer_dim, rngs=rngs),
+            nnx.LayerNorm(layer_dim, rngs=rngs),
+            nnx.relu,
+        )
+        self.actor_discrete = nnx.Linear(
+            layer_dim,
+            action_dim,
+            rngs=rngs,
+            kernel_init=nnx.initializers.orthogonal(jnp.sqrt(0.001)),
+        )
+        self.actor_mu = nnx.Linear(
+            layer_dim, 1, rngs=rngs, kernel_init=nnx.initializers.orthogonal(jnp.sqrt(0.001))
+        )
+        self.actor_std = nnx.Linear(
+            layer_dim, 1, rngs=rngs, kernel_init=nnx.initializers.orthogonal(jnp.sqrt(0.001))
+        )
+        self.critic = nnx.Sequential(
+            nnx.relu,
+            nnx.Linear(layer_dim, layer_dim, rngs=rngs),
+            nnx.LayerNorm(layer_dim, rngs=rngs),
+            nnx.relu,
+            nnx.Linear(
+                layer_dim,
+                1,
+                rngs=rngs,
+                kernel_init=nnx.initializers.orthogonal(jnp.sqrt(0.001)),
+            ),
+        )
+
+    def __call__(self, hidden_state, observation):
+        obs_emb = self.layer(observation)
+        next_state, output = self.gru(hidden_state, obs_emb)
+
+        # Actor
+        pi_out = self.actor(output)
+        logits = self.actor_discrete(pi_out)
+        mean = self.actor_mu(pi_out)
+        std = self.actor_mu(pi_out)
+
+        # Critic
+        value = self.critic(output)
+
+        return next_state, logits, mean, std, value
 
     def initialize_carry(self, shape):
         return self.gru.initialize_carry(shape)
