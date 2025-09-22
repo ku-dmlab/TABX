d41b585801ab8d4868383bd8376936fffd8625c6
hy-kiera <bb.kiera@gmail.com>
2025-09-22 08:36:32 +0000

    fix: exp: fix return type

--- src/experiments/train_bs_mappo.py
+++ src/experiments/train_bs_mappo.py
@@ -22,8 +22,8 @@ class Config:
     base_path: str = "./ckpt/tabs_bs_mappo"
     project_name: str = "tabs_bs_mappo"
     gpu_id: int = 0
-    total_iter: int = 50
-    iter_per_train_step: int = 10
+    iter_per_step: int = 150
+    total_train_iter: int = 10
     debug: bool = False
 
 
@@ -85,7 +85,7 @@ if __name__ == "__main__":
     # Agent
     mappo = MAPPO(config.battle, env)
     train_state = mappo.init_train_state(
-        jax.random.key(config.seed), config.total_iter * config.iter_per_train_step
+        jax.random.key(config.seed), config.iter_per_step * config.total_train_iter
     )
 
     # Create logs directory if it doesn't exist
@@ -95,7 +95,7 @@ if __name__ == "__main__":
     @jax.jit
     def train_fn(carry):
         def train_body(carry, _):
-            train_state = carry
+            (train_state,) = carry
             train_state, rollout_result = mappo.rollout(train_state, repeated_scenarios)
             train_state, train_info = mappo.train(train_state, rollout_result)
 
@@ -111,14 +111,14 @@ if __name__ == "__main__":
 
             result = {"train_info": train_info, "metric": metric}
 
-            return train_state, result
+            return (train_state,), result
 
-        carry, result = jax.lax.scan(train_body, carry, None, config.iter_per_train_step)
+        carry, result = jax.lax.scan(train_body, carry, None, config.iter_per_step)
 
         return carry, result
 
-    carry = train_state
-    for step in tqdm(range(config.total_iter)):
+    carry = (train_state,)
+    for step in tqdm(range(config.total_train_iter)):
         carry, result = train_fn(carry)
 
         # Log metrics
@@ -130,7 +130,7 @@ if __name__ == "__main__":
                     wandb_log[f"{result_key}/{key_}"] = jax.tree.map(lambda x: x[i], value)
             wandb.log(wandb_log)
 
-        mappo.save_state(carry[0], config.save_path + f"bs/mappo/{step}")
+        # mappo.save_state(carry[0], config.save_path + f"bs/mappo/{step}")
 
         np_result = jax.tree.map(lambda x: np.array(x).tolist(), result)
         with open(os.path.join(logs_dir, f"result_{step}.json"), "w") as f:
