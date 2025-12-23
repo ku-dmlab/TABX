fix: algo: average computed scores across agents

src/baseline/ued/plr_mappo_rnn.py
@@ -557,7 +557,10 @@ def make_train(config):
                         traj_batch.global_done, traj_batch.value, max_returns, advantages
                     )
                     sampler, _ = level_sampler.insert_batch(
-                        sampler, new_levels, scores, {"max_return": max_returns}
+                        sampler,
+                        new_levels,
+                        scores.reshape(config["NUM_ENVS"], -1).mean(axis=1),
+                        {"max_return": max_returns.reshape(config["NUM_ENVS"], -1).mean(axis=1)},
                     )
 
                     sample_state = sample_state.replace(
@@ -645,14 +648,19 @@ def make_train(config):
 
                     # Calculate scores
                     max_returns = jnp.maximum(
-                        level_sampler.get_levels_extra(sampler, level_inds)["max_return"],
-                        compute_max_returns(traj_batch.global_done, traj_batch.reward),
-                    )
+                        level_sampler.get_levels_extra(sampler, level_inds)["max_return"][:, None],
+                        compute_max_returns(traj_batch.global_done, traj_batch.reward).reshape(
+                            config["NUM_ENVS"], -1
+                        ),
+                    ).reshape(-1)
                     scores = compute_score(
                         traj_batch.global_done, traj_batch.value, max_returns, advantages
                     )
                     sampler = level_sampler.update_batch(
-                        sampler, level_inds, scores, {"max_return": max_returns}
+                        sampler,
+                        level_inds,
+                        scores.reshape(config["NUM_ENVS"], -1).mean(axis=1),
+                        {"max_return": max_returns.reshape(config["NUM_ENVS"], -1).mean(axis=1)},
                     )
 
                     sample_state = sample_state.replace(
@@ -748,7 +756,10 @@ def make_train(config):
                         traj_batch.global_done, traj_batch.value, max_returns, advantages
                     )
                     sampler, _ = level_sampler.insert_batch(
-                        sampler, child_levels, scores, {"max_return": max_returns}
+                        sampler,
+                        child_levels,
+                        scores.reshape(config["NUM_ENVS"], -1).mean(axis=1),
+                        {"max_return": max_returns.reshape(config["NUM_ENVS"], -1).mean(axis=1)},
                     )
 
                     sample_state = sample_state.replace(

src/baseline/ued/scores.py
@@ -33,8 +33,8 @@ def accumulate_rollout_stats(dones, metrics, *, time_average):
 
         return (sum_val, max_val, accum_val, step_count, episode_count), None
 
-    batch_size = dones.shape[0]
-    zeros = jax.tree_util.tree_map(lambda x: jnp.zeros_like(x[:, 0]), metrics)
+    batch_size = dones.shape[1]
+    zeros = jax.tree_util.tree_map(lambda x: jnp.zeros_like(x[0]), metrics)
     (sum_val, max_val, _, _, episode_count), _ = jax.lax.scan(
         iter,
         (
@@ -44,7 +44,7 @@ def accumulate_rollout_stats(dones, metrics, *, time_average):
             jnp.zeros(batch_size, dtype=jnp.uint32),
             jnp.zeros(batch_size, dtype=jnp.uint32),
         ),
-        (jnp.permute_dims(dones, (1, 0)), jnp.permute_dims(metrics, (1, 0))),
+        (dones, metrics),
     )
 
     mean_val = jax.tree_util.tree_map(lambda x: x / jnp.maximum(episode_count, 1), sum_val)
@@ -63,7 +63,7 @@ def compute_max_mean_returns_epcount(dones, rewards):
 
 def max_mc(dones, values, max_returns, incomplete_value=-jnp.inf):
     mean_scores, _, episode_count = accumulate_rollout_stats(
-        dones, max_returns[:, None] - values, time_average=True
+        dones, max_returns[None, :] - values, time_average=True
     )
     return jnp.where(episode_count > 0, mean_scores, incomplete_value)
 