c384776fb688f672051123dc703b2d5d42e6797a
hy-kiera <bb.kiera@gmail.com>
2026-03-25 12:12:34 +0000

    fix: log: include env param type into eval scenario name

--- src/baseline/ued/dr_mappo_rnn.py
+++ src/baseline/ued/dr_mappo_rnn.py
@@ -3,6 +3,7 @@ Based on JaxMARL Implementation of MAPPO
 """
 
 import hashlib
+import itertools
 import json
 import os
 from dataclasses import dataclass
@@ -180,7 +181,10 @@ def make_train(config):
             heuristic_param_names=heuristic_params * len(eval_scenarios),
             n_repeat=config["NUM_EVAL"],
         )
-        eval_scenarios = eval_scenarios * len(heuristic_params)
+        eval_scenarios = [
+            f"{x}_{y}_t{config['ENV_PARAM_TYPE']}"
+            for x, y in itertools.product(eval_scenarios, heuristic_params)
+        ]
         eval_env = TABX(cfg=tabx_config)
         eval_env = TABXLogWrapper(eval_env)
         eval_env = TABXEnemyHeuristicWrapper(eval_env)
